b5a94622156f55c2c33f1ab45acece90b5fc4f51
DESKTOP-V5ASPEC\User <minwoo5321@gmail.com>
2017-12-07 11:57:51 +0900

    prac
    prac

---
+++ MuiTest.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Input, { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+
+const styles = theme => ({
+    
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+
+    formControl: {
+        margin: theme.spacing.unit,
+        width: 100,
+        height : 150
+    },
+  
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2,
+        width : 100,
+        height : 50,
+        color : '#E74E51',
+        fontWeight : 800,
+        textAlign : "center",
+        textDecoration : "none",
+        '&:before': {
+            display: 'none',
+        },
+        '&:after': {
+            display: 'none',
+        }
+    },    
+
+    menuItem :{
+        fontWeight : 800,
+        width : 100,
+        hieght : 30,
+        padding : "10px 10px",
+        backgroundColor :"rgba(255, 255, 255, 1)",
+        boxShadow: "10px rgba(0,0,0,0.3)",
+        '&:hover': {
+            backgroundColor: 'gray',
+        }
+    },
+
+     menuItemTextMain :{
+        color : "#D06C8E"
+    }
+});
+
+
+
+
+
+class MuiTest extends React.Component {
+    
+
+
+    state = {
+        age: '',
+        name: 'hai',
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+    
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} autoComplete="off">
+        
+        <FormControl className={classes.formControl}>
+                <Select
+                    value={this.state.age}
+                    onChange={this.handleChange}
+                    displayEmpty
+                    name="age"
+                    className={classes.selectEmpty}
+                >
+                    <div value='' className={classes.menuItem}><span className={classes.menuItemTextMain}>전체</span></div>
+                    <div value={10} className={classes.menuItem}>상위랭커</div>
+                </Select>
+        </FormControl>
+        
+      </form>
+    );
+  }
+}
+
+MuiTest.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+
+export default withStyles(styles)(MuiTest);
